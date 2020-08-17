e53881f34fa5fbf9896b2327696bc6a0790a7651
Lucas Holt <luke@foolishgames.com>
2020-08-17 11:37:33 -0400

    Add a second demo app

---
+++ thymeleaf-webclient/src/main/java/com/capitaltg/reactiveapp/controller/HomeController.java
@@ -0,0 +1,36 @@
+package com.capitaltg.reactiveapp.controller;
+
+import com.capitaltg.reactiveapp.service.CategoryService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.spring5.context.webflux.IReactiveDataDriverContextVariable;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
+
+/**
+ * Display the app index page.
+ *
+ * @author Lucas Holt
+ */
+@Controller
+public class HomeController {
+
+    private final CategoryService categoryService;
+
+    public HomeController(final CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @RequestMapping("/")
+    public String index(final Model model) {
+
+        // Load Data from a Flux into the model so that it can be streamed.
+        IReactiveDataDriverContextVariable reactiveDataDrivenMode =
+                new ReactiveDataDriverContextVariable(categoryService.getCategories(), 1);
+
+        model.addAttribute("categories", reactiveDataDrivenMode);
+
+
+        return "index";
+    }
+}
---
+++ thymeleaf-webclient/src/main/java/com/capitaltg/reactiveapp/service/CategoryService.java
@@ -0,0 +1,35 @@
+package com.capitaltg.reactiveapp.service;
+
+import com.capitaltg.reactiveapp.domain.Category;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+
+/**
+ * This service pulls a list of categories from the MidnightBSD App Store REST API.
+ *
+ * @author Lucas Holt
+ * @see com.capitaltg.reactiveapp.config.WebClientConfig
+ */
+@Service
+public class CategoryService {
+
+    @Qualifier("appStoreWebClient")
+    @Autowired
+    private WebClient appStoreWebClient;
+
+    /**
+     * Get a list of software package categories in a reactive manner.  This would replace RestTemplate calls in
+     * a traditional spring app.
+     *
+     * @return
+     */
+    public Flux<Category> getCategories() {
+        return appStoreWebClient.get()
+                .uri("/category")
+                .retrieve()
+                .bodyToFlux(Category.class);
+    }
+}
